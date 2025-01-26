8066920fe82c90789384a341c40ebda16b4903c8
Rishad Geyushov <rishadmc2@icloud.com>
2025-01-26 18:08:07 +0300

    updated TestForm.java

--- src/test/java/TestForm.java
+++ src/test/java/TestForm.java
@@ -3,10 +3,15 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestForm {
+
+    static File file;
 
     @BeforeAll
     static void beforeAll() {
@@ -14,17 +19,20 @@ public class TestForm {
         Configuration.baseUrl = ("https://demoqa.com");
         Configuration.pageLoadStrategy = "eager";
         Configuration.holdBrowserOpen = true;
+        file = new File("src/test/resources/img.png");
 
     }
 
     @Test
     void fillFormTest() {
         open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
         $("#firstName").setValue("Rishad");
         $("#lastName").setValue("Geyushov");
         $("#userEmail").setValue("rishad@kk.com");
         $("#userNumber").setValue("9261937876");
-        $("[for='gender-radio-2']").click();
+        $("[for='gender-radio-1']").click();
         $("#dateOfBirthInput").click();
         $("#dateOfBirthInput").clear();
         $(".react-datepicker__year-select").selectOption("1999"); // Выбрать год
@@ -33,6 +41,13 @@ public class TestForm {
         $(".subjects-auto-complete__value-container input").setValue("Math").pressEnter();
         $(".subjects-auto-complete__value-container input").setValue("physics").pressEnter();
         $("[for='hobbies-checkbox-2']").click();
+        $("#uploadPicture").uploadFile(file);
+        $("#currentAddress").setValue("MeatGrinder street 13");
+        $("#state").click();
+        $("#state").$(byText("NCR")).click();
+        $("#city").click();
+        $("#city").$(byText("Delhi")).click();
+        $("#submit").click();
 
 
 
@@ -41,9 +56,21 @@ public class TestForm {
 
 
 
-//        $("#output #name").shouldHave(text("Alex"));
-//        $("#output #email").shouldHave(text("alex@egorov.com"));
-//        $("#output #currentAddress").shouldHave(text("Some street 1"));
-//        $("#output #permanentAddress").shouldHave(text("Another street 1"));
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Rishad Geyushov"));
+        $(".table-responsive").shouldHave(text("rishad@kk.com"));
+        $(".table-responsive").shouldHave(text("Male"));
+        $(".table-responsive").shouldHave(text("9261937876"));
+        $(".table-responsive").shouldHave(text("21 August,1999"));
+        $(".table-responsive").shouldHave(text("Maths, Physics"));
+        $(".table-responsive").shouldHave(text("Reading"));
+        $(".table-responsive").shouldHave(text("img.png"));
+        $(".table-responsive").shouldHave(text("MeatGrinder street 13"));
+        $(".table-responsive").shouldHave(text("NCR Delhi"));
+        $("#closeLargeModal").click();
+
+
+
+
     }
 }
